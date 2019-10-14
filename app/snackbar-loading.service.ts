16d5fa54839f84b1d990e2a2c2e7745d650270e2
CliffCrerar <cliff.crerar@gmail.com>
2019-10-14 02:30:55 +0200

    1 file modified

--- app/snackbar-loading.service.ts
+++ app/snackbar-loading.service.ts
@@ -5,15 +5,18 @@ import { SnackBarLoadingModel, SnackBarDataModel } from './snackbar.loading.mode
 @Injectable({ providedIn: 'root' })
 export class SnackbarLoadingService {
   icons = ['sync', 'donut_large', 'settings_backup_restore', 'replay', 'sentiment_satisfied_alt', 'gps_fixed', 'gps_not_fixed', 'refresh', 'data_usage', 'rotate_right', 'rotate_left', 'filter_tilt_shift'];
-  snackBarData;
+  private _snackBarData;
   private data = new SnackBarDataModel('This is a message', 'ACTION');
   constructor() {
-    this.snackBarData = new SnackBarLoadingModel(
+    this._snackBarData = new SnackBarLoadingModel(
       this.icons,
       this.data,
       'announcementMessage',
       false, 'sync_problem',
       this.icons[0]
     )
+  }
+  get snackBarData(): SnackBarLoadingModel{
+    return this._snackBarData
   }
 }
